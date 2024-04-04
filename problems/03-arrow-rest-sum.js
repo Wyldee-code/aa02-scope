bc76ce31c08e3d4914b07b60ef921376bf53ae5c
Wyldeliz <santoswyldeliz@gmail.com>
2024-04-03 21:49:52 -0700

    solved

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -11,7 +11,19 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// Your code here 
+// Your code here
+const arrowRestSum = (...args) => {
+  let sum = 0;
+  args.forEach(Element => {
+    sum += Element;
+  })
+  return sum;
+};
+
+//Examples:
+console.log(arrowRestSum(3,5,6)); // => 14
+console.log(arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
+console.log(arrowRestSum(0)); // => 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
