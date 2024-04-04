c85201faf8d03b454eeda87da3ccee2226a76ff0
Wyldeliz <santoswyldeliz@gmail.com>
2024-04-03 21:26:42 -0700

    solved using forEach

--- problems/02-arrow-my-map.js
+++ problems/02-arrow-my-map.js
@@ -18,7 +18,26 @@ console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 ***********************************************************************/
 
-// Your code here 
+// Your code here
+const arrowMyMap = (array, cb) => {
+  const result = []
+  //use Array.forEach for iteration.
+  array.forEach(element => {
+    //we have to push into a new array to get results
+    result.push(cb(element))
+  })
+// The function will return an array of new elements
+  return result
+}
+
+//Examples:
+let result1 = arrowMyMap([100, 25, 81, 64], Math.sqrt);
+console.log(result1);   // [ 10, 5, 9, 8 ]
+
+const yell = el => el.toUpperCase() + '!'
+
+let result2 = arrowMyMap(['run', 'Forrest'], yell);
+console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
