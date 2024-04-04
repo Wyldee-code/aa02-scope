efde0511b951d26d091cef98be19f40296b77a35
Wyldeliz <santoswyldeliz@gmail.com>
2024-04-03 22:11:53 -0700

    solved

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -10,8 +10,20 @@ arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
-// Your code here 
+// Your code here
+const arrowAvgValue = (array) => {
+  let sum = 0;
+for (let i = 0; i < array.length; i++){
+  sum += array[i];
+}
+return sum / array.length;
 
+}
+
+// example
+console.log(arrowAvgValue([10, 20])); // => 15
+console.log(arrowAvgValue([2, 3, 7])); // => 4
+console.log(arrowAvgValue([100, 60, 64])); // => 74.66666666666667
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
